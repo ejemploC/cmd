49dd4c3edc12a82c87722c07926b7f370c5d4111
“Cristian_Jimenez” <adsi@ADSI.(none)>
2013-04-25 10:34:51 -0500

    Adicion de archivos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmd::Application.config.secret_token = 'be4caa3656ed7a259b78b2ce5d1cc616a637c0db9e3e949472d98c735db248ce49831aec1bfa13a468ca03f321dfe4e665db5d07007ebebd968d7ba2d49394af'
